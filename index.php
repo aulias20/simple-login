c0b2b3b2785806260231c594e33afed398031154
aulias20 <47821480+aulias20@users.noreply.github.com>
2020-10-29 00:31:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -34,8 +34,9 @@
 			<div class="wrap-login100">
 				<div class="login100-form-title" style="background-image: url(images/bg-01.jpg);">
 					<span class="login100-form-title-1">
-						Sign In
+						Sign In 
 					</span>
+					<br>Hint: username "ifupnyk" pass "1995"</br>
 				</div>
 				<!-- MASUK KE LOGIN -->
 				<form class="login100-form validate-form" action="loginprocess.php" method="POST">

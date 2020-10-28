d53fcf2d5edf94827d72ca796970ec39c8bdf118
aulias20 <47821480+aulias20@users.noreply.github.com>
2020-10-29 00:19:40 +0700

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -1,16 +1,8 @@
 <?php
-session_start();
-$query = new mysqli("localhost","root","","logintoday");
-
 $user = $_POST['user'];
 $pass = $_POST['pass'];
 
-$data = mysqli_query($query,"SELECT*FROM data where user='$user' AND pass='$pass';") or die(mysqli_error($query));
-
-$cek = mysqli_num_rows($data);
-// $cek2 = mysqli_fetch_array($data);
-if ($cek>0) {
-	$_SESSION['user']=$user;
+if ($user == 'admin' && $pass == '123') {
 	$_SESSION['status']="login";
 	header("location:home.php?");
 }else{

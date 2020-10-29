1de2b0e8dab04731fdbcd7ca435faf5924c6b0ba
aulias20 <47821480+aulias20@users.noreply.github.com>
2020-10-29 11:06:11 +0700

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -3,7 +3,7 @@ session_start();
 $user = $_POST['user'];
 $pass = $_POST['pass'];
 
-if ($user == 'ifupnyk' && $pass == '1995') {
+if (($user == 'ifupnyk' || $user == 'IFUPNYK') && $pass == '1995') {
 	$_SESSION['user']=$user;
 	$_SESSION['status']="login";
 	header("location:home.php?");
